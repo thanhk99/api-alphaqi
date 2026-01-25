2d0514180abc14c107d4bacdef01d37ee216cd27
thanhk99 <nhuthanh040804@gmail.com>
2026-01-25 16:25:22 +0700

    redeploy

--- src/main/java/course/model/RefreshToken.java
+++ src/main/java/course/model/RefreshToken.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Entity
 @Table(name = "refresh_tokens")
